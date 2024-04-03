df0f0ad366af41f4ce521eea1893aeb0cb4a2d12
RTIN <118107221+Romo3@users.noreply.github.com>
2024-04-03 10:21:35 -0400

    init test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./global.css";
+import Header from "./components/Header"
+import FormSelection from "./components/FormSelection"
+import Footer from "./components/Footer"
 
 function App() {
-  return <div className="text-2xl text-blue-950 font-semibold">MS - Forms</div>;
+  return <div className="text-2xl text-blue-950 font-semibold justify-center text-center py-8">
+      <Header/>
+        <div className="">
+          <FormSelection/>
+        </div>
+      <Footer/>
+    </div>;
 }
 
 export default App;
---
+++ src/components/FormSelection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormSelection = () => {
+  return (
+    <div>FormSelection</div>
+  )
+}
+
+export default FormSelection
