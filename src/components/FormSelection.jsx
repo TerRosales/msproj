001b5f50893695576b36e43a28476e44671ed912
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-04-04 13:47:38 -0400

    header, formSelection init

--- src/components/FormSelection.jsx
+++ src/components/FormSelection.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./componentStyles/FormSelection.css";
 const FormSelection = () => {
   return (
-    <div>FormSelection</div>
-  )
-}
+    <div>
+      <ul className="grid grid-rows-4 grid-flow-col w-15 justify-center m-5">
+        <li className="formSelectionList">Wash</li>
+        <li className="formSelectionList">Gears</li>
+        <li className="formSelectionList">Basement</li>
+        <li className="formSelectionList">191</li>
+        <li className="formSelectionList">200</li>
+        <li className="formSelectionList">110</li>
+        <li className="formSelectionList">600</li>
+        <li className="formSelectionList">200</li>
+      </ul>
+    </div>
+  );
+};
 
-export default FormSelection
+export default FormSelection;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <div className="flex justify-center">
-      <h1>MS - Forms&nbsp;</h1>
-      <span className="bg-gradient-to-b from-blue-100 to-blue-500 p-1">
+      <h1 className="mt-4">MS - Forms&nbsp;</h1>
+      <span className="bg-gradient-to-b from-blue-100 to-blue-400 p-1">
+        <span className="flex rounded text-sm bg-yellow-100 text-red-700 hover:bg-red-300 hover:text-black">
+          Current Location
+        </span>
         Wash
       </span>
     </div>
